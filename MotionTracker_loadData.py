7f31473c99b9d55bc44ca79b668f8f548b8ea40a
Jason Coleman <jcoleman@ufl.edu>
2017-11-04 09:06:03 -0400

    Adapted for new multi-ROI MotionTracker code

--- MotionTracker_loadData.py
+++ MotionTracker_loadData.py
@@ -24,26 +24,35 @@ def load_data():
     with open(file_path) as f:  # Python 3: open(..., 'rb')
         alldata = pickle.load(f)
         a = alldata['frametracker']
-        b = alldata['frameNumber']
+        b = alldata['frametracker2']
+        c = alldata['frametracker3']
+        d = alldata['frameNumber']
         #pad with nans/zeros to align frames (frameNumber - len(a))
         
-    return file_path, alldata, a, b
+    return file_path, alldata, a, b, c, d
 
     
-# Check data
-filepath_right, data_right, trackframes_right, totalframes_right = load_data()
-frame_offset_right = totalframes_right-len(trackframes_right)
-data_right = np.concatenate((np.zeros(frame_offset_right), trackframes_right), 0)
+## Check data
+#filepath_right, data_right, trackframes_right, totalframes_right = load_data()
+#frame_offset_right = totalframes_right-len(trackframes_right)
+#data_right = np.concatenate((np.zeros(frame_offset_right), trackframes_right), 0)
 
 #filepath_mid, data_mid, trackframes_mid, totalframes_mid = load_data()
 #frame_offset_mid = totalframes_mid-len(trackframes_mid)
 #data_mid = np.concatenate((np.zeros(frame_offset_mid), trackframes_mid), 0)
 
-data_rightShift = np.array(data_right)+1
+#NEED TO ADD OFFSET FOR FRAMES, THEN IGNORE FIRST 25*30 sec?
+#frame_offset_mid = totalframes_mid-len(trackframes_mid)
+#data_mid = np.concatenate((np.zeros(frame_offset_mid), trackframes_mid), 0)
 
-plt.plot(data_rightShift, 'b')
-#plt.plot(data_mid, 'r')
+data_rightShift = np.array(frametracker)+1
+data_leftShift = np.array(frametracker3)-1
+
+plt.plot(frametracker, 'g')
+plt.plot(frametracker2, 'b')
+plt.plot(frametracker3, 'r')
 plt.ylim([-2,4])
 plt.title('tracked in right chamber')
-#plt.legend({'R', 'M'})
+plt.legend({'Right frametracker','Mid frametracker2', 'Left frametracker3'})
+
 
